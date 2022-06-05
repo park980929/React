05380733b1ba696bd3aaeebc594ee4039cf7dee2
wonwoopark <qkrdnjsdn99@naver.com>
2022-06-05 14:55:36 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  let post = '강남 우동 맛집';
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
+  let[여자코트, 여자코드변경] = useState('여자코드 변경');
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="black-nav">
+        <h4> ReactBlog </h4> 
+      </div>
+
+      <div className ='list'>
+        <h4> {글제목[0]}  <span onClick={()=>{ 따봉변경(따봉+1)}}>👍</span> 
+        {따봉} </h4>
+        <p>2월 17일 발행</p>
+      </div>   
+
+      <div className ='list'>
+        <h4> {글제목[1]} 
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>  
+
+      <div className ='list'>
+        <h4> {글제목[2]} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+    <Modal></Modal>
+      
+
+
     </div>
   );
 }
 
+function Modal() {
+  return (
+      <div className='model'>
+        <h4> 제목 </h4>
+        <p> 날짜 </p>
+        <p> 상세내용 </p>
+      </div >
+  )
+}
 export default App;
